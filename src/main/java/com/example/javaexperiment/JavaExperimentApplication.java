97ceaf9a3ca0c8813bed6424261d507eddcd2857
liawom@foxmail.com <60684451+Aw0m@users.noreply.github.com>
2021-10-20 23:45:30 +0800

    Initial commit

---
+++ src/main/java/com/example/javaexperiment/JavaExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExperimentApplication.class, args);
+    }
+
+}
