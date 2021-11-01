fe381dcddd9c8acd20eb4cf77726a430420bec88
liawom@foxmail.com <60684451+Aw0m@users.noreply.github.com>
2021-11-01 23:53:10 +0800

    发表文章的Controller

--- src/main/java/com/example/javaexperiment/controller/PublishController.java
+++ src/main/java/com/example/javaexperiment/controller/PublishController.java
@@ -1,8 +1,24 @@
 package com.example.javaexperiment.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author: Awom
  * @Date: 2021/10/23 0:08
  */
+
+@RestController
+@RequestMapping("/articles")
 public class PublishController {
+    @GetMapping("/new_article")
+    public String newArticle(String articleContent) {
+
+    }
+
+    @GetMapping("/delete_article")
+    public String deleteArticle(String articleID) {
+
+    }
 }
